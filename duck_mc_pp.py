c73933f86a114612e8bdce3d6b6b7a1a0453f135
Mislav <mislav@vingd.com>
2016-11-15 10:18:43 +0100

    experiment 10B d

--- duck_mc_pp.py
+++ duck_mc_pp.py
@@ -21,24 +21,22 @@ def duck_mc_experiment(E_12, broj_ponavljanja, postav_index):
         'postav_3': {'distance': [0.35875,0.35675,0.35775,0.35975], 'theta': [46.49, 26.00, 33.00, 53.00], 'phi': [0, 0, 180, 180], 'num_stripes': 16, 'detector_width': 0.05}
         }    
         
-    """ PRVI SLUČAJ: 10B + 10B -> 10B + 10B -> 6Li + 4He + 10B
-    """  
-    
-    postav_index = 'postav_3'
 
     kwargs = {
         "E_p": 72.132,
         "m_p": 10.,
         "m_t": 10.,
-        "m_X": 10.,
-        "m_E": 10.,
-        "m_C": 6.,
-        "m_D": 4.,
-        "Q_01": 0., # Q_0 za reakciju 1
-        "E_prag": 4.461,
-        "Emin_C": [17.5, 18.0, 16.6, 16.2], #za det na 0st
-        "Emin_D": [9.4, 9.7, 8.9, 8.7],
+        "m_X": 12.,
+        "m_E": 8.,
+        "m_C": 10.,
+        "m_D": 2.,
+        "Q_01": 19.15971, # Q_0 za reakciju 1
+        "E_prag": 25.1868,
+        "Emin_C": [37.4, 38.6, 35.4, 34.5], #za 10B za det na 0st
+        "Emin_D": [3.2, 3.3, 3.0, 3.0] #za d, za det na 0 st
+        
     }
+
 
     
     detectors = [duck_mc.define_detector(POSTAV, i, postav_index) for i in range(4)]
@@ -54,7 +52,7 @@ def duck_mc_experiment(E_12, broj_ponavljanja, postav_index):
 
 
 
-BROJ_RADILICA = 30
+BROJ_RADILICA = 24 
 
 job_server = pp.Server(secret='ZekoPeko&%!', ncpus=BROJ_RADILICA)
 print "Starting pp with", job_server.get_ncpus(), "workers"
@@ -65,12 +63,12 @@ MIJENJAMO ENERGIJU E_12 OD 0 DO E_MAX_REL
 E_12 - energija iznad praga koji je potreban za raspad X -> C + D
 """
 E_MAX_REL = 30.0
-E_12_STEP = 0.05
+E_12_STEP = 0.1
 E_12 = 0. 
-BROJ_PONAVLJANJA = 10000000
+BROJ_PONAVLJANJA = 1000000
 #BROJ_PONAVLJANJA = 100000
 
-POSTAV = 'postav_3'
+POSTAV = 'postav_1'
 
 while E_12 <= E_MAX_REL:
 
@@ -88,7 +86,7 @@ while E_12 <= E_MAX_REL:
 
 
 bufsize = 1
-target = open("Monte-Carlo-elasticno-postav3-10M.csv", "w", bufsize)
+target = open("Monte-Carlo-10Bd-postav1-1M.csv", "w", bufsize)
 
 # dohvacamo rezultate analiza
 for job in jobs:
